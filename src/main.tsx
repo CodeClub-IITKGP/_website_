b344c07c064a3c4a05fbb19abbb6a579eaca8d49
Tanishq <tanishqsaxena.in@gmail.com>
2026-02-03 20:26:38 +0530

    Use HashRouter to fix GitHub Pages refresh 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import "./App.css";
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/_website_">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
